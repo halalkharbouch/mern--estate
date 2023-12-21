7064f967890bea807af08570f1f528ca22fa1d8f
halalkharbouch <binyaminbgod@gmail.com>
2023-12-22 00:33:41 +0100

    create middleware function to handle possible errors

--- api/index.js
+++ api/index.js
@@ -17,5 +17,14 @@ app.use(express.json())
 
 app.use('/api/user', userRoter)
 app.use('/api/auth', authRouter)
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Eror';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  })
+})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
