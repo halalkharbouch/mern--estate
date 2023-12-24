2aa1fdb2c6e375a72659f8df378353d506606be6
halalkharbouch <binyaminbgod@gmail.com>
2023-12-24 16:19:04 +0100

    create update user api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose
 const app = express();
 
 app.use(express.json())
+app.use(cookieParser())
+
 
 app.use('/api/user', userRoter)
 app.use('/api/auth', authRouter)
@@ -26,5 +29,7 @@ app.use((err, req, res, next) => {
     message,
   })
 })
+
+
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
---
+++ api/utils/verifyUser.js
@@ -0,0 +1,15 @@
+import { erorHandler } from "./error.js";
+import jwt from 'jsonwebtoken'
+
+export const verifyToken = (req, res, next) => {
+    const token = req.cookies.access_token;
+    
+
+    if(!token) return next(erorHandler(401, 'Unauthorized'))
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err) return next(erorHandler(403, 'Forbidden'))
+        req.user = user;
+        next();
+    })
+}
