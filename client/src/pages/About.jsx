3baa73c18cdac647d6e978ead3a0134c45033410
halalkharbouch <binyaminbgod@gmail.com>
2023-12-27 14:38:57 +0100

    Update index.html and About.jsx

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+
+      <h1 className='text=3xl font-bold mb-4 text-slate-800'>About Halal Estate</h1>
+      <p className='mb-4 text-slate-700'>Halal Estate is a leading real estate agency that specializes in helping clients
+        buy, sell and rent properties in the most desirable neighborhoods, Our team of 
+        experienced real estate agents is ready to help you find homes for sale that meet
+        all of your criteria. We are a team of experienced, licensed real estate agents
+        serving the New York City metro area. With a proven track record of getting results
+        quickly and a direct line of communication at all times.</p>
+      <p className='mb-4 text-slate-700'>
+        We are a team of experienced, licensed real estate agents serving the New York City
+        metro area. With a proven track record of getting results quickly and a direct line
+        of communication at all times. We are a team of experienced, licensed real estate
+        agents serving the New York City metro area. With a proven track record of getting
+      </p>
+      <p className='mb-4 text-slate-700'>
+        We are a team of experienced, licensed real estate agents serving the New York City
+        metro area. With a proven track record of getting results quickly and a direct line
+        of communication at all times.
+
+      </p>
+
+    </div>
   )
 }
